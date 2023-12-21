df1b611e123757d57be722a4dd866fcbda794361
David Schramm <david.schramm@live.com>
2023-12-21 18:53:52 +0100

    add eicar

--- src/messagebox.c
+++ src/messagebox.c
@@ -1,13 +1,15 @@
 #include <stdio.h>
 #include <windows.h>
 
+#define EICAR "X5O!P%@AP[4\\PZX54(P^)7CC)7}$EICAR-STANDARD-ANTIVIRUS-TEST-FILE!$H+H*"
+
 void display_box(const char *title, const char *text) {
   MessageBox(NULL, text, title, MB_OK);
 }
 
 int main(int ac, char *as[]){
-  puts("What's up?");
-  display_box("Message", ac>1?as[1]:"test 123");
+  puts(EICAR);
+  display_box("Message", EICAR);
   
   return 66;
 }
